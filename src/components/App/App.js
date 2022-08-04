d9c66171989f10d0dd4fb4ae53f3b2ea29f365c4
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-04 14:20:50 +0300

    Static Movies Added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,49 @@
-import './App.css';
+import React from 'react';
 
-function App() {
+import './App.css';
+class App extends React.Component {
+  
+  state = {
+
+    movies : [
+
+      {
+        "id": 1,
+        "name": "The Flash",
+        "rating": 8.3,
+        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
+      },
+    
+      {
+        "id": 2,
+        "name": "Interstellar",
+        "rating": 6.8,
+        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
+      },
+    
+      {
+        "id": 3,
+        "name": "Arrow",
+        "rating": 7.9,
+        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
+      }
+    
+    ]
+
+  }
+
+  render () {
     return (
       <div className="App">
         <p>Hello World</p>
       </div>
     );
   }
+
+}
 
 
 export default App;
