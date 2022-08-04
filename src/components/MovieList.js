6ab649c27dedff9ced87d5854b76a7bc76a35b32
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-04 17:07:59 +0300

    Converted movieList class based to function based

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,19 +2,19 @@ import React from "react";
 
 // import './MovieList.css';
 
-class MovieList extends React.Component {
-    render() {
-        return (
-            <div className="row">
+const MovieList = (props) => {
+
+    return (
+        <div className="row">
 
 
-                {this.props.movies.map((movie) => (
-                    <div className="col-lg-4">
+            {props.movies.map((movie) => (
+                <div className="col-lg-4" key={movie.id}>
                     <div className="card mb-4 shadow-sm">
                         <img
-                            src= {movie.imageURL}
+                            src={movie.imageURL}
                             className="card-img-top"
-                            alt= {movie.name}
+                            alt={movie.name}
                         />
                         <div className="card-body">
                             <h5 className="card-title">
@@ -36,11 +36,11 @@ class MovieList extends React.Component {
                         </div>
                     </div>
                 </div>
-                ) )}
+            ))}
 
-            </div>
-        )
-    }
+        </div>
+    )
 }
+
 
 export default MovieList;
