f8404fe3361098fa9f17a1589211a9e67d92c6eb
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-05 12:55:32 +0300

    Added local api

--- src/components/App.js
+++ src/components/App.js
@@ -5,66 +5,46 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 
+
+/*
+HTTP Method
+
+
+Get -> For get data
+Post -> For add data
+Put -> For update data
+Patch -> For update data
+Delete -> For delete data
+
+*/
+
+// Postman -> Use for http request
+
 class App extends React.Component {
   
   state = {
 
+    // Fetch -> Fetch is a JavaScript function that allows us to perform asynchronous network queries and returns a promise back to us.
+
     movies : [
 
-      {
-        "id": 1,
-        "name": "The Flash",
-        "rating": 8.3,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-      },
-    
-      {
-        "id": 2,
-        "name": "Interstellar",
-        "rating": 6.8,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-      },
-    
-      {
-        "id": 3,
-        "name": "Arrow",
-        "rating": 7.9,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-      } ,
-
-      {
-        "id": 4,
-        "name": "Rogue",
-        "rating": 7.4,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg"
-    },
-
-    {
-        "id": 5,
-        "name": "Project Power",
-        "rating": 6.7,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg"
-    },
-
-    {
-        "id": 6,
-        "name": "Superman",
-        "rating": 7.6,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
-    }
-    
     ] ,
     
     search : ""
 
 
 
+  }
+
+
+  async componentDidMount () {
+    const baseUrl = "http://localhost:3002/movies";
+    const response = await fetch(baseUrl);   // response is a promise
+    const data = await response.json();
+    // console.log(data);
+    this.setState({
+      movies : data
+    });
   }
 
 
@@ -84,14 +64,14 @@ class App extends React.Component {
   }
 
   searchMovie = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     this.setState( {
       search : event.target.value
     })
   }
 
 
-  render () {
+  render () {   // use only view operation
 
     let filteredMovies = this.state.movies.filter (
       (movie) => {
